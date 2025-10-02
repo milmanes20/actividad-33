041cec96652634108e9473f88fbe0a08a7ab091b
Marco Castillo Cabrera <marco.cast.cab@gmail.com>
2025-10-02 15:23:49 -0300

    mod App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-//import './App.css'
+import './App.css'
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Register from "./components/Register";
